e485ed5e84bf49e6999046c2bc1466f82c3778e2
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-09-05 14:02:16 -0400

    Command line tooling

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject phonebook "0.1.0-SNAPSHOT"
   :description "Command line phonebook tool"
   :url "http://github.com/madhuvishy/phonebook"
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main phonebook.core
+  )
--- src/phonebook/core.clj
+++ src/phonebook/core.clj
@@ -1,13 +1,5 @@
 (ns phonebook.core
-  (:require [clojure.java.shell :as shell])
   (:gen-class))
-
-(defn create-phonebook 
-  "Create new phonebook"
-  [phonebook-name]
-  (do
-    (shell/sh "touch" (str "./" phonebook-name))
-    (str "Created phonebook " phonebook-name " in the current directory.")))
 
 (defn exists?
   "Check if phonebook exists"
@@ -16,11 +8,84 @@
     (when (slurp phonebook-name) true)
     (catch Exception e false)))
 
-(defn add-to-phonebook
+(defn create-phonebook 
+  "Create new phonebook"
+  [[phonebook-name]]
+  (if (exists? phonebook-name)
+    "Phonebook with same name already exists!"
+    (do
+      (spit (str "./" phonebook-name) "")
+      (str "Created phonebook " phonebook-name " in the current directory."))))
+
+(defn add-entry
   "Add entry to phonebook"
-  [name number phonebook-name]
+  [[name number phonebook-name]]
+  (if (exists? phonebook-name)
+    (let [phonebook (read-string (slurp phonebook-name))]
+        (if (phonebook name) "Entry already exists"
+          (spit phonebook-name (pr-str (assoc phonebook name number)))))
+    "Phonebook does not exist"))
+
+(defn change-entry
+  "Change an entry in the phonebook"
+  [[name number phonebook-name]]
   (if (exists? phonebook-name)
     (let [phonebook (read-string (slurp phonebook-name))]
         (if (phonebook name)
-          "Entry already exists"
-          (spit phonebook-name (pr-str (assoc phonebook name number)))))))
+          (spit phonebook-name (pr-str (assoc phonebook name number))))
+          "Entry doesn't exist")    
+    "Phonebook does not exist"))
+
+(defn remove-entry
+  "Remove an entry in the phonebook"
+  [[name phonebook-name]]
+  (if (exists? phonebook-name)
+    (let [phonebook (read-string (slurp phonebook-name))]
+        (if (phonebook name)
+          (spit phonebook-name (pr-str (dissoc phonebook name))))
+          "Entry doesn't exist")    
+    "Phonebook does not exist"))
+
+(defn lookup
+  "Lookup an entry on a phonebook"
+  [[name phonebook-name]]
+  (if (exists? phonebook-name)
+    (let [phonebook (read-string (slurp phonebook-name))
+          names (keys phonebook)]
+       
+      )
+    "Phonebook does not exist"))
+
+(defn reverse-lookup
+  "Lookup an entry on a phonebook"
+  [[name phonebook-name]]
+  (if (exists? phonebook-name)
+    (let [phonebook (read-string (slurp phonebook-name))
+          names (keys phonebook)]
+       
+      )
+    "Phonebook does not exist"))
+
+(defn help
+  "Help for command line tool"
+  []
+  (str "Usage:\n"
+       "create <phonebook-name>\n"
+       "add <entry-name> <entry-number> <phonebook-name>\n"
+       "change <entry-name> <entry-number> <phonebook-name>\n"
+       "remove <entry-name> <phonebook-name>\n"
+       "lookup <name-search-string> <phonebook-name>\n"
+       "reverse-lookup <number-search-string> <phonebook-name>\n"))
+
+(defn -main 
+  "Main function"
+  [& args]
+  (let [command (first args)]
+    (println (condp = command
+      "help" (help)
+      "create" (create-phonebook (rest args))
+      "add" (add-entry (rest args))
+      "change" (change-entry (rest args))
+      "remove" (remove-entry (rest args))
+      "lookup" (lookup (rest args))
+      "reverse-lookup" (reverse-lookup (rest args))))))
