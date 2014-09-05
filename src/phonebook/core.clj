85c60c718eb01fc775103ff9ede97ae310dca4a8
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-09-05 13:16:57 -0400

    Functions for create, add, change, remove entries work

--- src/phonebook/core.clj
+++ src/phonebook/core.clj
@@ -1,6 +1,26 @@
-(ns phonebook.core)
+(ns phonebook.core
+  (:require [clojure.java.shell :as shell])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn create-phonebook 
+  "Create new phonebook"
+  [phonebook-name]
+  (do
+    (shell/sh "touch" (str "./" phonebook-name))
+    (str "Created phonebook " phonebook-name " in the current directory.")))
+
+(defn exists?
+  "Check if phonebook exists"
+  [phonebook-name]
+  (try 
+    (when (slurp phonebook-name) true)
+    (catch Exception e false)))
+
+(defn add-to-phonebook
+  "Add entry to phonebook"
+  [name number phonebook-name]
+  (if (exists? phonebook-name)
+    (let [phonebook (read-string (slurp phonebook-name))]
+        (if (phonebook name)
+          "Entry already exists"
+          (spit phonebook-name (pr-str (assoc phonebook name number)))))))
