929653ab70db99099c0d2848966ba387b3ad9704
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-09-05 12:16:12 -0400

    Initial template files

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject phonebook "0.1.0-SNAPSHOT"
+  :description "Command line phonebook tool"
+  :url "http://github.com/madhuvishy/phonebook"
+  :dependencies [[org.clojure/clojure "1.5.1"]])
