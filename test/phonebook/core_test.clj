6f052a002e98260dee21d529f07277133fdbeea5
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-09-05 16:49:18 -0400

    A few tests

--- test/phonebook/core_test.clj
+++ test/phonebook/core_test.clj
@@ -2,6 +2,23 @@
   (:require [clojure.test :refer :all]
             [phonebook.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-create-phonebook
+  (testing "Create-new-phonebook"
+    (is (= (create-phonebook ["testpb.pb"]) 
+           "Created phonebook testpb.pb in the current directory."))
+    (is (= (create-phonebook ["testpb.pb"])
+           "Phonebook with same name already exists!"))))
+
+(deftest test-add-entry
+  (testing "Adding new entry"
+    (is (= (add-entry ["Madhu Vishy" "9084940482" "testpb.pb"])
+           "Entry added"))
+    (is (= (add-entry ["Madhu Vishy" "9084940482" "testpb.pb"])
+           "Entry already exists"))))
+
+(deftest test-change-entry
+  (testing "Changing an entry"
+    (is (= (change-entry ["Madhu Vishy" "9084040482" "testpb.pb"])
+           "Entry changed"))
+    (is (= (change-entry ["Madhu" "9084940482" "testpb.pb"])
+           "Entry already exists"))))
